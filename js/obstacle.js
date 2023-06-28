2b04b2d4c20398f42b87c9126e8044528297a287
Sophia Ziesch <s.ziesch@posteo.de>
2023-06-28 14:26:19 +0200

    fixed the position-range of the falling obstacles

--- js/obstacle.js
+++ js/obstacle.js
@@ -1,22 +1,18 @@
 class Obstacle {
 	constructor(gameScreen, speed) {
 		this.gameScreen = gameScreen;
-		this.left = Math.floor(Math.random() * 300 + 70);
+		this.left = Math.floor(Math.random() * 520);
 		this.top = -150;
 		this.width = 80;
 		this.height = 150;
 		this.speed = speed;
 		this.element = document.createElement("img");
-
 		this.element.src = "./img/comet.png";
 		this.element.style.position = "absolute";
-
 		this.element.style.width = `${this.width}px`;
 		this.element.style.height = `${this.height}px`;
-
 		this.element.style.top = `${this.top}px`;
 		this.element.style.left = `${this.left}px`;
-
 		this.gameScreen.appendChild(this.element);
 	}
 
